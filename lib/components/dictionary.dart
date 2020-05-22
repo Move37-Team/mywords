32d9f5bb7b06118c0fe94995cdfba7db1844bb2d
Ali Parvizi <snparvizi75@gmail.com>
2020-05-22 10:48:05 +0430

    :recycle: abstracted dictionary related stuff into Dictionary class

---
+++ lib/components/dictionary.dart
@@ -0,0 +1,159 @@
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:mutex/mutex.dart';
+
+import 'package:flutter/services.dart';
+import 'package:tuple/tuple.dart';
+
+import 'library.dart';
+
+class Dictionary {
+  /// An actual english dictionary
+  /// consists of three parts
+  /// 1 - Webster's Unabridged English Dictionary
+  /// 2 - WordNet thesaurus - used for synonyms
+  /// 3 - A trie structured list of words - used for fast word lookups
+  /// all the files are in json format and in assets folder and
+  /// will be loaded into native dart Maps
+
+  bool _dictLoaded = false;                                               // if dictionary is loaded
+  bool _trieLoaded = false;                                               // if trie dict file is loaded
+  bool _synonymsLoaded = false;                                           // is synonym file is loaded
+  bool _allLoaded = false;                                                // if all of the three dictionary files are loaded
+
+  Map<String, dynamic> _dictionary;                                       // the global dictionary
+  Map<String, dynamic> _trieDict;                                         // the global dictionary keys in trie structure
+  Map<String, dynamic> _synonymsDict;                                     // dictionary of word synonyms
+
+  Function callback;                                                     // when the dictionary is loaded into memory, this callback will be called
+  Mutex m = Mutex();                                                     // used to ensure loaded statuses are set correctly
+
+  get loaded => _allLoaded;
+
+  Dictionary({ @required this.callback}) {
+    _loadAll();
+  }
+
+  Future<void> _loadJson(String path, Function callback) async {
+    // load and parse json from assets and return the results via callback
+    String jsonString =  await rootBundle.loadString(path);
+    Map<String, dynamic> dict = jsonDecode(jsonString);
+    callback(dict);
+  }
+
+  void _updateAllLoadedStatus() async {
+    await m.acquire();
+
+    // set allLoaded
+    _allLoaded = _dictLoaded && _trieLoaded && _synonymsLoaded;
+
+    if (_allLoaded)
+      callback();
+
+    m.release();
+  }
+
+
+  void _loadAll() {
+    // load all three dictionary files
+
+    // load Webster's Unabridged English Dictionary
+    _loadJson("assets/dictionary_web.json", (dict) {
+      _dictLoaded = true;
+      _dictionary = dict;
+      _updateAllLoadedStatus();
+    });
+
+    // load trie file
+    _loadJson("assets/trie_dict.json", (dict) {
+      _trieLoaded = true;
+      _trieDict = dict;
+      _updateAllLoadedStatus();
+    });
+
+    // load synonyms file
+    _loadJson("assets/synonyms_dict.json", (dict) {
+      _synonymsLoaded = true;
+      _synonymsDict = dict;
+      _updateAllLoadedStatus();
+    });
+
+  }
+
+  List<SingleWord> searchAndUpdateList(String word)  {
+    // search for the given word in the dictionary
+    // also shows every possible word starting with the given word
+    // if nothing is found, the given word will be returned
+
+    // search results
+    List<SingleWord> wordsFound = List();
+
+    if (!_allLoaded || word.length == 0)
+      return wordsFound;
+
+    // regardless of word being found, add it to the list
+    wordsFound.add(
+        SingleWord(
+            word: word,
+            definition: _dictionary[word],
+
+            // the flag is set to false, but one should change it the appropriate value upon using it
+            // it's set to false because I want to keep the coupling with other classes to minimum
+            isInFavoriteList: false
+        )
+    );
+
+    // the node that the search starts with, it's based on the first character of the given word
+    Map<String, dynamic> root;
+
+    try {
+      root = _trieDict[word[0]];
+    } catch (E) {
+      return wordsFound;
+    }
+
+    // traverse the word tree up to the given word first
+    Map<String, dynamic> currentNode = root;
+    for (var i = 1 ; i < word.length; i ++) {
+      try {
+        currentNode = currentNode[word[i]];
+      } catch (E) {
+        return wordsFound;
+      }
+    }
+
+    // find all the possible words starting from this word
+    List<Tuple2<String, Map<String, dynamic>> > stack = List();  // (baseWord, node)
+
+    currentNode.keys.forEach((element) {
+      if ( currentNode[element].runtimeType != String ) {
+        stack.add(Tuple2(word + element, currentNode[element]));
+      }
+    });
+
+    while(stack.length > 0) {
+      // pop an item from the top of the stack
+      var top = stack[stack.length - 1];
+      stack.removeLast();
+
+      if (top.item2.containsKey("_end_"))
+        wordsFound.add(
+            SingleWord(
+                word: top.item1,
+                definition: _dictionary[top.item1],
+                isInFavoriteList: false
+            )
+        );
+
+      // add the current nodes children with the new word as their baseWord to be checked
+      top.item2.keys.forEach((element) {
+        if ( top.item2[element].runtimeType != String ) {
+          stack.add(Tuple2(top.item1 + element, top.item2[element]));
+        }
+      });
+    }
+
+    return wordsFound;
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
-import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:flutter/services.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:mywords/components/library.dart';
 import 'package:mywords/components/wordTIle.dart';
@@ -11,6 +9,7 @@ import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:tuple/tuple.dart';
 
 import 'components/debounce.dart';
+import 'components/dictionary.dart';
 
 void main() async {
   runApp(MyApp());
@@ -44,126 +43,26 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
   final WordLibrary _library = WordLibrary();                             // user's custom words selection library
   final _wordTextController = TextEditingController();                    // search text field controller
   List<SingleWord> _showingWords = List();                                // list of the words that are shown in the list view
-  bool _dictLoaded = false;                                               // if dictionary is loaded
-  Map<String, dynamic> _dictionary;                                       // the global dictionary
-  Map<String, dynamic> _trieDict;                                         // the global dictionary keys in trie structure
-
+  Dictionary _dictionary;                                                 // actual english dictionary
   RefreshController _refreshController =                                  // refresh controller
   RefreshController(initialRefresh: true);
   final _debounce = Debounce(milliseconds: 500);                          // search field debounce
 
-  _MyHomePageState() {
-    _loadDict();
-  }
+  @override
+  initState() {
+    super.initState();
 
-  Future<bool> _loadDict() async {
-    // load english dictionary from json asset
-    // load and parse json dictionary
-    String dictJsonString =  await rootBundle.loadString("assets/dictionary_web.json");
-    String trieJsonString =  await rootBundle.loadString("assets/trie_dict.json");
-
-    setState(() {
-      _dictionary = jsonDecode(dictJsonString);
-      _trieDict = jsonDecode(trieJsonString);
-    });
-
-    setState(() {
-      _dictLoaded = true;
-    });
+    _dictionary = Dictionary(callback: () { setState(() {}); });
 
   }
 
-  Future<void> searchAndUpdateList(String word) async {
-    // search for the given word in the dictionary
-    // also shows every possible word starting with the given word
-    // if nothing is found, the given word will be returned
-
-    if (!_dictLoaded || word.length == 0)
-      return;
-
-    // search results
-    List<SingleWord> wordsFound = List();
-
-    // regardless of word being found, add it to the list
-    wordsFound.add(
-        SingleWord(
-            word: word,
-            definition: _dictionary[word],
-            isInFavoriteList: _library.contains(word)
-        )
-    );
-
-    // the node that the search starts with, it's based on the first character of the given word
-    Map<String, dynamic> root;
-
-    try {
-      root = _trieDict[word[0]];
-    } catch (E) {
-      setState(() {
-        _showingWords = wordsFound;
-      });
-
-      return;
-    }
-
-    // traverse the word tree up to the given word first
-    Map<String, dynamic> currentNode = root;
-    for (var i = 1 ; i < word.length; i ++) {
-      try {
-        currentNode = currentNode[word[i]];
-      } catch (E) {
-        setState(() {
-          _showingWords = wordsFound;
-        });
-
-        return;
-      }
-    }
-
-
-
-    // find all the possible words starting from this word
-    List<Tuple2<String, Map<String, dynamic>> > stack = List();  // (baseWord, node)
-
-    currentNode.keys.forEach((element) {
-      if ( currentNode[element].runtimeType != String ) {
-        stack.add(Tuple2(word + element, currentNode[element]));
-      }
-    });
-
-    while(stack.length > 0) {
-      // pop an item from the top of the stack
-      var top = stack[stack.length - 1];
-      stack.removeLast();
-
-      if (top.item2.containsKey("_end_"))
-        wordsFound.add(
-          SingleWord(
-            word: top.item1,
-            definition: _dictionary[top.item1],
-            isInFavoriteList: _library.contains(top.item1)
-          )
-        );
-
-      // add the current nodes children with the new word as their baseWord to be checked
-      top.item2.keys.forEach((element) {
-        if ( top.item2[element].runtimeType != String ) {
-          stack.add(Tuple2(top.item1 + element, top.item2[element]));
-        }
-      });
-    }
-
-    // update the show words list
-    setState(() {
-      _showingWords = wordsFound;
-    });
-  }
 
   Future<void> doRefresh() async {
     // if the search field is not empty refresh based on dictionary data
     if (_wordTextController.text.trim().length > 0) {
-      await searchAndUpdateList(_wordTextController.text.trim().toLowerCase());
-
+      setState(() {
+        _showingWords = _dictionary.searchAndUpdateList(_wordTextController.text.trim().toLowerCase());
+      });
     } else {
       // if search filed is empty reload user's favorite words from database
       await _library.loadFromDatabase();
@@ -235,12 +134,15 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
               Container(
                 padding: EdgeInsets.all(10),
                 child: TextField(
-                  enabled: _dictLoaded ? true : false,
+                  enabled: _dictionary.loaded ? true : false,
                   controller: _wordTextController,
                   onChanged: (text) {
                     _debounce.run(() {
                       if (_wordTextController.text.length > 0){
-                        searchAndUpdateList(text.trim().toLowerCase());
+                        setState(() {
+                          _showingWords = _dictionary.searchAndUpdateList(_wordTextController.text.trim().toLowerCase());
+
+                        });
                       }
                       else {
                         setState(() {
@@ -255,7 +157,7 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
                   ),
                   decoration: InputDecoration(
                     fillColor: Colors.white,
-                    hintText: _dictLoaded ? 'Enter a word to search or add' : 'Loading dictionary ...',
+                    hintText: _dictionary.loaded ? 'Enter a word to search or add' : 'Loading dictionary ...',
                     focusedBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.all(Radius.circular(10.0)),
                         borderSide: BorderSide(style: BorderStyle.solid, color: Colors.black12)
@@ -283,6 +185,7 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
                     onLoading: _onLoading,
                     child: ListView.builder (
                       itemBuilder: (BuildContext context, int index) {
+                        _showingWords[index].isInFavoriteList = _library.contains(_showingWords[index].word);
                         return Card(
                           key: Key(_showingWords[index].word),
                           child: WordTile( word: _showingWords[index], adder: _add, remover: _remove,)
